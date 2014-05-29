49f099f4f2361fd21d345f1196aa277514e48094
Blazhukas <alpheratz93@gmail.com>
2014-05-30 00:27:24 +0300

    added sysfs calculation cause
    added sysfs calculation cause

--- calc.c
+++ calc.c
@@ -13,8 +13,7 @@
 #include <linux/proc_fs.h>
 #endif
 
-#define WRITE_SIZE 100
-
+// names
 #define ARG1 "arg1"
 #define ARG2 "arg2"
 #define OPERATION "operation"
@@ -22,65 +21,16 @@
 
 #define PARENT_DIR "calc"
 
-struct proc_dir_entry *calc_dir;
-struct proc_dir_entry *arg1;
-struct proc_dir_entry *arg2;
-struct proc_dir_entry *operation;
-struct proc_dir_entry *result;
 
+#define WRITE_SIZE 100
 
 static char arg1_input[WRITE_SIZE];
 static char arg2_input[WRITE_SIZE];
 static char operation_input[WRITE_SIZE];
 
-/*
- * arg1 write handler
- */
-int write_arg1(struct file *file, const char *buf, unsigned long count, void *data)
-{
-	if(count > WRITE_SIZE) {
-    	count = WRITE_SIZE;
-	}
 
-	//memset(arg1_input, 0, WRITE_SIZE);
-	memcpy(arg1_input, buf, count);
-	return count;
-}
 
-/*
- * arg2 write handler
- */
-int write_arg2(struct file *file, const char *buf, unsigned long count, void *data)
-{
-	if(count > WRITE_SIZE) {
-    	count = WRITE_SIZE;
-	}
-
-	//memset(arg2_input, 0, WRITE_SIZE);
-	memcpy(arg2_input, buf, count);
-	return count;
-}
-
-/*
- * operation write handler
- */
-int write_operation(struct file *file, const char *buf, unsigned long count, void *data)
-{
-	if(count > WRITE_SIZE) {
-    	count = WRITE_SIZE;
-	}
-
-	//memset(operation, 0, WRITE_SIZE);
-	memcpy(operation_input, buf, count);
-	return count;
-}
-
-/*
- * result read handler
- */
-int read_result(char *buffer, char **buffer_location,
-                  off_t offset, int buffer_length, int *eof, void *data)
-{
+long calculate(void) {
 	long a1 = 0;
 	long a2 = 0;
 	long res = 0;
@@ -101,6 +51,169 @@ int read_result(char *buffer, char **buffer_location,
 	} else if (operation_input[0] == '/') {
 		res = a1 / a2;
 	}
+	return res;
+}
+
+#ifdef SYSFS
+
+struct calc_attr {
+	struct attribute attr;
+	int value;
+};
+
+static struct attribute arg1 = {
+	.name = ARG1,
+	.mode = 0644,
+};
+
+static struct attribute arg2 = {
+	.name = ARG2,
+	.mode = 0644,
+};
+
+static struct attribute operation = {
+	.name = OPERATION,
+	.mode = 0644,
+};
+
+static struct attribute result = {
+	.name = RESULT,
+	.mode = 0644,
+};
+
+static struct attribute * calc_attributes[] = {
+	&arg1,
+	&arg2,
+	&operation,
+	&result,
+	NULL
+};
+
+
+static ssize_t default_show(struct kobject *kobj, struct attribute *attr,
+		char *buf)
+{
+	if (!strcmp(attr->name, RESULT)) {
+		long res = calculate();
+
+		return sprintf(buf, "%ld\n", res);
+	} else {
+		return 0;
+	}
+}
+
+static ssize_t default_store(struct kobject *kobj, struct attribute *attr,
+		const char *buf, size_t len)
+{
+	if(len > WRITE_SIZE) {
+		len = WRITE_SIZE;
+	}
+
+	if (!strcmp(attr->name, ARG1)) {
+		memcpy(arg1_input, buf, len);
+	} else if (!strcmp(attr->name, ARG2)) {
+		memcpy(arg2_input, buf, len);
+	} else if (!strcmp(attr->name, OPERATION)) {
+		memcpy(operation_input, buf, len);
+	}
+	return len;
+}
+
+static struct sysfs_ops calc_ops = {
+	.show = default_show,
+	.store = default_store,
+};
+
+static struct kobj_type calc_type = {
+	.sysfs_ops = &calc_ops,
+	.default_attrs = calc_attributes,
+};
+
+struct kobject *calc_obj;
+static int __init sysfsexample_module_init(void)
+{
+	int err = -1;
+	calc_obj = kzalloc(sizeof(*calc_obj), GFP_KERNEL);
+	if (calc_obj) {
+		kobject_init(calc_obj, &calc_type);
+		if (kobject_add(calc_obj, NULL, "%s", PARENT_DIR)) {
+			 err = -1;
+			 printk("Sysfs creation failed\n");
+			 kobject_put(calc_obj);
+			 calc_obj = NULL;
+		}
+		err = 0;
+	}
+	return err;
+}
+
+static void __exit sysfsexample_module_exit(void)
+{
+	if (calc_obj) {
+		kobject_put(calc_obj);
+		kfree(calc_obj);
+	}
+}
+
+module_init(sysfsexample_module_init);
+module_exit(sysfsexample_module_exit);
+MODULE_LICENSE("GPL");
+
+
+#else
+
+struct proc_dir_entry *calc_dir;
+struct proc_dir_entry *arg1;
+struct proc_dir_entry *arg2;
+struct proc_dir_entry *operation;
+struct proc_dir_entry *result;
+
+/*
+ * arg1 write handler
+ */
+int write_arg1(struct file *file, const char *buf, unsigned long count, void *data)
+{
+	if(count > WRITE_SIZE) {
+		count = WRITE_SIZE;
+	}
+
+	memcpy(arg1_input, buf, count);
+	return count;
+}
+
+/*
+ * arg2 write handler
+ */
+int write_arg2(struct file *file, const char *buf, unsigned long count, void *data)
+{
+	if(count > WRITE_SIZE) {
+		count = WRITE_SIZE;
+	}
+
+	memcpy(arg2_input, buf, count);
+	return count;
+}
+
+/*
+ * operation write handler
+ */
+int write_operation(struct file *file, const char *buf, unsigned long count, void *data)
+{
+	if(count > WRITE_SIZE) {
+		count = WRITE_SIZE;
+	}
+
+	memcpy(operation_input, buf, count);
+	return count;
+}
+
+/*
+ * result read handler
+ */
+int read_result(char *buffer, char **buffer_location,
+				  off_t offset, int buffer_length, int *eof, void *data)
+{
+	long res = calculate();
 
 	return sprintf(buffer, "%ld\n", res);
 }
@@ -117,33 +230,33 @@ int init_module()
 	// arg1
 	arg1 = create_proc_entry(ARG1, 0666, calc_dir);
 	if(!arg1) {
-    	printk(KERN_INFO "Error creating proc entry");
-    	return -ENOMEM;
-    }
+		printk(KERN_INFO "Error creating proc entry");
+		return -ENOMEM;
+	}
 	arg1->write_proc = write_arg1;
 
 	// arg2
 	arg2 = create_proc_entry(ARG2, 0666, calc_dir);
 	if(!arg2) {
-    	printk(KERN_INFO "Error creating proc entry");
-    	return -ENOMEM;
-    }
+		printk(KERN_INFO "Error creating proc entry");
+		return -ENOMEM;
+	}
 	arg2->write_proc = write_arg2;
 
 	// operation
 	operation = create_proc_entry(OPERATION, 0666, calc_dir);
 	if(!operation) {
-    	printk(KERN_INFO "Error creating proc entry");
-    	return -ENOMEM;
-    }
+		printk(KERN_INFO "Error creating proc entry");
+		return -ENOMEM;
+	}
 	operation->write_proc = write_operation;
 
 	// result
 	result = create_proc_entry(RESULT, 0666, calc_dir);
 	if(!result) {
-    	printk(KERN_INFO "Error creating proc entry");
-    	return -ENOMEM;
-    }
+		printk(KERN_INFO "Error creating proc entry");
+		return -ENOMEM;
+	}
 	result->read_proc = read_result;
 
 	printk(KERN_INFO "/proc/%s created\n", PARENT_DIR);
@@ -159,3 +272,4 @@ void cleanup_module()
 	printk(KERN_INFO "/proc/%s removed\n", PARENT_DIR);
 }
 
+#endif
